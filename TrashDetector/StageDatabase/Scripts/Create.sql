e948d0ecc0f6158823f8517231cf0d2aadcec930
Christoffer Sørensen <Christofferlund92@hotmail.com>
2018-04-03 21:00:59 +0200

    Added a crude setup for the stage DB

---
+++ TrashDetector/StageDatabase/Scripts/Create.sql
@@ -0,0 +1,13 @@
+﻿USE TStageDatabase
+
+CREATE TABLE ImagesTable AS FileTable
+WITH (
+ FileTable_Directory = 'ImagesDirectory',
+ FileTable_Collate_Filename = database_default
+)
+
+CREATE TABLE ImagesInformation (
+	locatio VARCHAR(24),
+	timee VARCHAR(24),
+	isTrash BIT
+)
