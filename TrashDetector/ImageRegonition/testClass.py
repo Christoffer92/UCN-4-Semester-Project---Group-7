cdfc9d381ed524670b5491ad765b6c49d813e14c
Christoffer Sørensen <Christofferlund92@hotmail.com>
2018-04-18 22:14:06 +0200

    minor work and tested it abit, it takes about 20minutes to rebuild the network graph and only about 2sec to predict an image with the graph.

--- TrashDetector/ImageRegonition/testClass.py
+++ TrashDetector/ImageRegonition/testClass.py
@@ -1,8 +1,16 @@
+import datetime
+
+print('Starting import stuff: ', datetime.datetime.now())
+
 import train
 import predict
 
 
+print('Start: ', datetime.datetime.now())
 
-#train.train(1000)
-
+#train.train(5000)
 predict.runPridict()
+
+print('Finished: ', datetime.datetime.now())
+
+
